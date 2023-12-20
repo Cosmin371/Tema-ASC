2e2e796ab4446a8d513d7514439ea03417c92535
Cosmin371 <57593806+Cosmin371@users.noreply.github.com>
2023-12-20 14:37:55 +0200

    Cerinta 0x02 TERMINATA

--- 151_Glod_CosminStefan_2.s
+++ 151_Glod_CosminStefan_2.s
@@ -1,10 +1,7 @@
 .data
 	s: .space 1600 #18 linii si coloane +2 pentru matricea extinsa
 	s1: .space 1600 #copie
-	formatScanf: .asciz "%ld"
-	formatPrintf: .asciz "%ld "
-	newLine: .asciz "\n"
-	intreMatrice: .asciz "--------\n"
+	
 	n: .space 4
 	n2: .space 4
 	m: .space 4
@@ -17,15 +14,20 @@
 	coloana: .space 4
 	k: .space 4
 	vecini: .space 4
+	dimMat: .space 4
 	
-	
-	input_file: .ascii "in.txt"
-	output_file: .ascii "out.txt"
+	input_file: .asciz "in.txt"
+	output_file: .asciz "out.txt"
 
 	pointer_input_file: .long 0
 	pointer_output_file: .long 0
 
 	w: .asciz "w"
+	r: .asciz "r"
+	
+	formatScanf: .asciz "%ld"
+	formatPrintf: .asciz "%ld "
+	newLine: .asciz "\n"
 .text
 .global main
 main:
@@ -36,25 +38,39 @@ main:
 	popl %ebx
 
 	movl %eax, pointer_output_file  # pointerul catre fisierul de iesire
+
+	pushl $r
+	pushl $input_file
+	call fopen
+	popl %ebx
+	popl %ebx
 	
+	movl %eax, pointer_input_file	# pointerul catre fisierul de intrare
+
 	#cin >> n
 	pushl $n
 	pushl $formatScanf
-	call scanf
+	pushl pointer_input_file
+	call fscanf
+	popl %ebx
 	popl %ebx
 	popl %ebx
 
 	#cin >> m
 	pushl $m
 	pushl $formatScanf
-	call scanf
+	pushl pointer_input_file
+	call fscanf
+	popl %ebx
 	popl %ebx
 	popl %ebx
 
 	#cin >> p
 	pushl $p
 	pushl $formatScanf
-	call scanf
+	pushl pointer_input_file
+	call fscanf
+	popl %ebx
 	popl %ebx
 	popl %ebx
 
@@ -67,33 +83,20 @@ main:
 	movl %eax, m2
 	addl $2, n2
 	addl $2, m2
+	
+	xorl %edx, %edx
+	movl n2, %eax
+	mull m2
+	movl %eax, dimMat
 
+xorl %ecx, %ecx				#initializare matrice s cu 0
 initializare_matrice:
-	# initializez toata matricea extinsa s cu 0
-	movl $0, linie
-	parcurgere_linie:
-		movl linie, %ecx
-		cmp n2, %ecx
-		je sfarsit_initializare_matrice
-
-		movl $0, coloana
-		parcurgere_coloana:
-			movl coloana, %ecx
-			cmp m2, %ecx
-			je sfarsit_parcurgere_coloana
-
-			movl linie, %eax
-			xor %edx, %edx
-			mull n2
-			addl coloana, %eax
-
-			movl $0, (%edi, %eax, 4)
-
-			incl coloana
-			jmp parcurgere_coloana
-		sfarsit_parcurgere_coloana:
-		incl linie
-		jmp parcurgere_linie
+	cmp %ecx, dimMat
+	je sfarsit_initializare_matrice
+	
+	movl $0, (%edi, %ecx, 4)
+	incl %ecx
+	jmp initializare_matrice
 sfarsit_initializare_matrice:
 
 movl $0, index
@@ -109,14 +112,18 @@ loop_citire:
 	#cin >> left
 	pushl $left
 	pushl $formatScanf
-	call scanf
+	pushl pointer_input_file
+	call fscanf
+	popl %ebx
 	popl %ebx
 	popl %ebx
 
 	#cin >> right
 	pushl $right
 	pushl $formatScanf
-	call scanf
+	pushl pointer_input_file
+	call fscanf
+	popl %ebx
 	popl %ebx
 	popl %ebx
 
@@ -124,7 +131,7 @@ loop_citire:
 	movl left, %eax
 	incl %eax
 	xor %edx, %edx
-	mull n2
+	mull m2
 	addl right, %eax
 	incl %eax
 	movl $1, (%edi, %eax, 4)
@@ -136,7 +143,9 @@ sfarsit_loop_citire:
 citire_k:       # cin >> k
 	pushl $k
 	pushl $formatScanf
-	call scanf
+	pushl pointer_input_file
+	call fscanf
+	popl %ebx
 	popl %ebx
 	popl %ebx
 sfarsit_citire_k:
@@ -147,32 +156,14 @@ for_k_loop:
 	cmpl k, %ecx
 	je sfarsit_for_k_loop
 
-	resetare_matrice_s1:
-	# initializez toata matricea extinsa s1 cu 0
-	movl $0, linie
-	parcurgere_linie1:
-		movl linie, %ecx
-		cmp n2, %ecx
+	xorl %ecx, %ecx
+	resetare_matrice_s1:		# initializez toata matricea extinsa s1 cu 0
+		cmp %ecx, dimMat
 		je sfarsit_resetare_matrice_s1
-
-		movl $0, coloana
-		parcurgere_coloana1:
-			movl coloana, %ecx
-			cmp m2, %ecx
-			je sfarsit_parcurgere_coloana1
-
-			movl linie, %eax
-			xor %edx, %edx
-			mull n2
-			addl coloana, %eax
-
-			movl $0, (%esi, %eax, 4)
-
-			incl coloana
-			jmp parcurgere_coloana1
-		sfarsit_parcurgere_coloana1:
-		incl linie
-		jmp parcurgere_linie1
+		
+		movl $0, (%esi, %ecx, 4)
+		incl %ecx
+		jmp resetare_matrice_s1
 	sfarsit_resetare_matrice_s1:
 	
 	movl $1, linie
@@ -193,7 +184,7 @@ for_k_loop:
 			# %eax = [linie, coloana]
 			movl linie, %eax
 			xor %edx, %edx
-			mull n2
+			mull m2
 			addl coloana, %eax
 			
 			# %eax = [linie, coloana +1 -1]
@@ -205,7 +196,7 @@ for_k_loop:
 			addl %ecx, vecini
 			
 			# %eax = [linie - 1, coloana +0 +1 -1]
-			subl n2, %eax #scad o linie
+			subl m2, %eax #scad o linie
 			movl (%edi, %eax, 4), %ecx
 			addl %ecx, vecini
 			decl %eax
@@ -216,8 +207,8 @@ for_k_loop:
 			addl %ecx, vecini
 			
 			# %eax = [linie + 1, coloana +0 +1 -1]
-			addl n2, %eax
-			addl n2, %eax
+			addl m2, %eax
+			addl m2, %eax
 			movl (%edi, %eax, 4), %ecx
 			addl %ecx, vecini
 			incl %eax
@@ -228,9 +219,9 @@ for_k_loop:
 			addl %ecx, vecini
 			
 			# inapoi la pozitia initiala [linie, coloana]
-			subl n2, %eax
+			subl m2, %eax
 			decl %eax
-			
+
 			cmpl $1, (%edi, %eax, 4)
 			je celula_vie
 			
@@ -266,37 +257,17 @@ for_k_loop:
 		incl linie
 		jmp pentru_linie		
 	sfarsit_pentru_linie:
-
-	# s = s1
 	
+	xorl %ecx, %ecx			# copiere matrice s1 inapoi in s
 	copiere_matrice:
-	movl $1, linie
-	copiere_linie:
-		movl linie, %ecx
-		cmpl n, %ecx
-		jg sfarsit_copiere_linie
+		cmp %ecx, dimMat
+		je sfarsit_copiere_matrice
 		
-		movl $1, coloana
-		copiere_coloana:
-			movl coloana, %ecx
-			cmpl m, %ecx
-			jg sfarsit_copiere_coloana
-			
-			movl linie, %eax
-			xor %edx, %edx
-			mull n2
-			addl coloana, %eax
-			
-			movl (%esi, %eax, 4), %ecx
-			movl %ecx, (%edi, %eax, 4)
-			
-			incl coloana
-			jmp copiere_coloana
-		sfarsit_copiere_coloana:
-
-		incl linie
-		jmp copiere_linie
-	sfarsit_copiere_linie:
+		movl (%esi, %ecx, 4), %eax
+		movl %eax, (%edi, %ecx, 4)
+		
+		incl %ecx
+		jmp copiere_matrice
 	sfarsit_copiere_matrice:
 
 	incl index
@@ -319,7 +290,7 @@ afisare_matrice:
 
 			movl linie, %eax
 			xor %edx, %edx
-			mull n2
+			mull m2
 			addl coloana, %eax
 
 			pushl (%edi, %eax, 4)
@@ -338,15 +309,20 @@ afisare_matrice:
 			jmp for_columns
 
 	new_line:
-	movl $4, %eax
-	movl $1, %ebx
-	movl $newLine, %ecx
-	movl $2, %edx
-	int $0x80
+	pushl $newLine
+	pushl pointer_output_file
+	call fprintf
+	popl %ebx
+	popl %ebx
+	
+	pushl $0
+	call fflush
+	popl %ebx
 
 	incl linie
 	jmp for_lines
 sfarsit_afisare_matrice:
+jmp exit
 
 exit:
 	movl $1, %eax
